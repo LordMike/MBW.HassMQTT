20b3234e6a1cfe9940e826081630319325335901
Michael Bisbjerg <michael@mbwarez.dk>
2023-08-06 23:24:00 +0200

    Update for latest docs
    https://github.com/home-assistant/home-assistant.io/compare/dafff4dff7a45ddb0df4994da1aa3d5e1e05d5e7..83c7ad319da1eac5c9a72e3665e0829e1f3a4a55

--- MBW.HassMQTT.DiscoveryModels/Models/MqttVacuumState.cs
+++ MBW.HassMQTT.DiscoveryModels/Models/MqttVacuumState.cs
@@ -1,133 +1,15 @@
 ﻿#nullable enable
-using System.Collections.Generic;
-using FluentValidation;
-using JetBrains.Annotations;
-using MBW.HassMQTT.DiscoveryModels.Availability;
-using MBW.HassMQTT.DiscoveryModels.Enum;
-using MBW.HassMQTT.DiscoveryModels.Interfaces;
-using MBW.HassMQTT.DiscoveryModels.Metadata;
+using System;
 
 namespace MBW.HassMQTT.DiscoveryModels.Models;
 
 /// <summary>
-/// https://www.home-assistant.io/integrations/vacuum.mqtt/#state-configuration
-/// 
-/// The mqtt vacuum integration allows you to control your MQTT-enabled vacuum. There are two possible message
-/// schemas - legacy and state, chosen by setting the schema configuration parameter. New installations should
-/// use the state schema as legacy is deprecated and might be removed someday in the future. The state schema
-/// is preferred because the vacuum will then be represented as a StateVacuumDevice which is the preferred parent
-/// vacuum entity.
-///
-/// The initial state of the state vacuum entity will set to `unknown` and can be reset by a device by sending
-/// a `null` payload as state. The legacy `mqtt` vacuum does not support handling an `unknown` state.
+/// Migrate to <see cref="MqttVacuum"/>
 /// </summary>
-[DeviceType(HassDeviceType.Vacuum)]
-[PublicAPI]
-public class MqttVacuumState : MqttSensorDiscoveryBase<MqttVacuumState, MqttVacuumState.MqttVacuumStateValidator>, IHasUniqueId, IHasAvailability, IHasQos, IHasJsonAttributes, IHasIcon, IHasEnabledByDefault, IHasRetain, IHasEntityCategory, IHasObjectId
+[Obsolete("Migrate to 'MqttVacuum'")]
+public class MqttVacuumState : MqttVacuum 
 {
     public MqttVacuumState(string discoveryTopic, string uniqueId) : base(discoveryTopic, uniqueId)
     {
-    }
-
-    /// <summary>
-    /// The MQTT topic to publish commands to control the vacuum.
-    /// </summary>
-    public string? CommandTopic { get; set; }
-
-    /// <summary>
-    /// List of possible fan speeds for the vacuum.
-    /// </summary>
-    public IList<string>? FanSpeedList { get; set; }
-
-    /// <summary>
-    /// The name of the vacuum.
-    /// </summary>
-    public string? Name { get; set; }
-
-    /// <summary>
-    /// The payload to send to the `command_topic` to begin a spot cleaning cycle.
-    /// </summary>
-    public string? PayloadCleanSpot { get; set; }
-
-    /// <summary>
-    /// The payload to send to the `command_topic` to locate the vacuum (typically plays a song).
-    /// </summary>
-    public string? PayloadLocate { get; set; }
-
-    /// <summary>
-    /// The payload to send to the `command_topic` to pause the vacuum.
-    /// </summary>
-    public string? PayloadPause { get; set; }
-
-    /// <summary>
-    /// The payload to send to the `command_topic` to tell the vacuum to return to base.
-    /// </summary>
-    public string? PayloadReturnToBase { get; set; }
-
-    /// <summary>
-    /// The payload to send to the `command_topic` to begin the cleaning cycle.
-    /// </summary>
-    public string? PayloadStart { get; set; }
-
-    /// <summary>
-    /// The payload to send to the `command_topic` to stop cleaning.
-    /// </summary>
-    public string? PayloadStop { get; set; }
-
-    /// <summary>
-    /// The schema to use. Must be `state` to select the state schema.
-    /// </summary>
-    public string? Schema { get; set; } = "state";
-
-    /// <summary>
-    /// The MQTT topic to publish custom commands to the vacuum.
-    /// </summary>
-    public string? SendCommandTopic { get; set; }
-
-    /// <summary>
-    /// The MQTT topic to publish commands to control the vacuum's fan speed.
-    /// </summary>
-    public string? SetFanSpeedTopic { get; set; }
-
-    /// <summary>
-    /// The MQTT topic subscribed to receive state messages from the vacuum. Messages received on the `state_topic` must be a valid JSON dictionary, with a mandatory `state` key and optionally `battery_level` and `fan_speed` keys as shown in the [example](#state-mqtt-protocol).
-    /// </summary>
-    public string? StateTopic { get; set; }
-
-    /// <summary>
-    /// List of features that the vacuum supports, possible values are:
-    /// - start
-    /// - stop
-    /// - pause
-    /// - return_home
-    /// - battery
-    /// - status
-    /// - locate
-    /// - clean_spot
-    /// - fan_speed
-    /// - send_command
-    /// </summary>
-    public IList<string>? SupportedFeatures { get; set; }
-
-    public string? UniqueId { get; set; }
-    public IList<AvailabilityModel>? Availability { get; set; }
-    public AvailabilityMode? AvailabilityMode { get; set; }
-    public MqttQosLevel? Qos { get; set; }
-    public string? JsonAttributesTemplate { get; set; }
-    public string? JsonAttributesTopic { get; set; }
-    public string? Icon { get; set; }
-    public bool? EnabledByDefault { get; set; }
-    public bool? Retain { get; set; }
-    public EntityCategory? EntityCategory { get; set; }
-    public string? ObjectId { get; set; }
-
-    public class MqttVacuumStateValidator : MqttSensorDiscoveryBaseValidator<MqttVacuumState>
-    {
-        public MqttVacuumStateValidator()
-        {
-            RuleFor(s => s.FanSpeedList)
-                .NotEmpty()
-                .When(s => s.FanSpeedList != null);
-        }
     }
 }
